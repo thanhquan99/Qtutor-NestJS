8193f17ca8adc4b228cdcff7425fed0a5e6858e4
ThanhQuan <thanhquan050399@gmail.com>
2021-02-12 23:40:48 +0700

    Set up swagger UI

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { CinemasModule } from './cinemas/cinemas.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), CinemasModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), CinemasModule, UsersModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cinema Api')
+    .setDescription('The Cinema API')
+    .setVersion('1.0')
+    .addTag('cinemas')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
