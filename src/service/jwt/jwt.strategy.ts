78fe6b765e6577e986a9bbbe4dd69d0004fc49ec
ThanhQuan <thanhquan050399@gmail.com>
2021-03-09 09:46:13 +0700

    Add create user for user api

---
+++ src/service/jwt/jwt.strategy.ts
@@ -0,0 +1,31 @@
+import { JwtPayload } from './jwt-payload.interface';
+import { UserRepository } from './../../users/user.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UnauthorizedException } from '@nestjs/common';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import * as CONFIG from 'config';
+const jwtConfig = CONFIG.get('jwt');
+
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET || jwtConfig.secret,
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    const { username } = payload;
+    const user = this.userRepository.findOne({ username });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
