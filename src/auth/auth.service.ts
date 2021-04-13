0b6dfb49220dc36eef536c8b5a4b7687ae792826
ThanhQuan <thanhquan050399@gmail.com>
2021-04-13 21:36:34 +0700

    Fix ACID transaction

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -11,7 +11,7 @@ import {
 import { InjectRepository } from '@nestjs/typeorm';
 import { JwtService } from '@nestjs/jwt';
 import { JwtPayload } from 'src/service/jwt/jwt-payload.interface';
-import { getConnection } from 'typeorm';
+import { getConnection, getManager } from 'typeorm';
 import { Role } from 'src/roles/role.entity';
 import { UserRole } from 'src/user-role/userRole.entity';
 
@@ -28,38 +28,34 @@ export class AuthService {
       throw new BadRequestException('Email is already exist');
     }
 
-    let user;
     const queryRunner = getConnection().createQueryRunner();
     await queryRunner.connect();
 
     await queryRunner.startTransaction();
 
     try {
-      // execute some operations on this transaction:
-      user = queryRunner.manager.create(User, registerUserDto);
-      await user.hashPassword();
-      await user.save();
-      const role = await queryRunner.manager.findOne(Role, {
-        name: RoleName.CUSTOMER,
+      await getManager().transaction(async (entityManager) => {
+        const user = entityManager.create(User, registerUserDto);
+        await user.hashPassword();
+        await entityManager.save(user);
+
+        const role = await entityManager.findOne(Role, {
+          id: 12,
+          name: RoleName.CUSTOMER,
+        });
+
+        await entityManager
+          .create(UserRole, {
+            user,
+            role,
+          })
+          .save();
       });
-      const userRole = await queryRunner.manager
-        .create(UserRole, {
-          user,
-          role,
-        })
-        .save();
-      // commit transaction now:
-      await queryRunner.commitTransaction();
     } catch (err) {
-      // since we have errors let's rollback changes we made
-      await queryRunner.rollbackTransaction();
       throw new BadRequestException(`Failed due to ${err}`);
-    } finally {
-      // you need to release query runner which is manually created:
-      await queryRunner.release();
     }
+
     return {
-      user,
       message: 'Register success',
     };
   }
