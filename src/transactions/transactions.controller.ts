502cf55e9e7ed36aa77b32f88795b47a3e8aab44
sangnh99 <sangnh99@gmail.com>
2021-04-28 17:08:54 +0700

    entity transaction

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,5 +1,8 @@
 import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { BaseControllerCRUD } from 'src/base/base-controller-CRUD';
+import { Permissions } from 'src/guards/permissions.decorator';
+import { PermissionAction } from 'src/permissions/permission.entity';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { Transaction } from './transactions.entity';
 import { TransactionsService } from './transactions.service';
 
@@ -10,9 +13,9 @@ export class TransactionsController extends BaseControllerCRUD<Transaction>{
     }
 
     @Post()
-    @Permissions(PermissionAction.CREATE_RATING)
+    @Permissions(PermissionAction.CREATE_TRANSACTION)
     @UsePipes(ValidationPipe)
-    createOne(@Body() createDto: CreateRatingDto): Promise<Rating> {
+    createOne(@Body() createDto: CreateTransactionDto): Promise<Transaction> {
       return this.service.createOne(createDto);
     }
 }
--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BaseServiceCRUD } from 'src/base/base-service-CRUD';
+import { Transaction } from './transactions.entity';
 
 @Injectable()
-export class TransactionsService {}
+export class TransactionsService extends BaseServiceCRUD<Transaction> {
+    constructor(@InjectRepository(Transaction) repo) {
+        super(repo,Transaction, "Transaction");
+      }
+}
